feadbf8dc57ab2a3d577ed0289bf5bc65be25e21
Alihan Doğuş Yalçın <alihandogus.yalcin@masomo.com>
2021-01-21 20:39:44 +0300

    url update

--- nats-spy.rb
+++ nats-spy.rb
@@ -1,7 +1,7 @@
 class NatsSpy < Formula
    desc "nats-spy is a terminal tool to help you to monitor NATS messages."
    homepage "https://github.com/alihanyalcin/nats-spy"
-   url "https://github.com/alihanyalcin/nats-spy/releases/download/v0.1.0/nats-spy.tar.gz"
+   url "https://github.com/alihanyalcin/nats-spy/releases/download/v0.1.0/nats-spy-mac.tar.gz"
    sha256 "94949978f9d7a1208276d2ee743692520a452956532d0ca6854c9c36850a3e6f"
    version "v0.1.0"
 
