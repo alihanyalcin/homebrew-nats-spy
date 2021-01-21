3d1427287c933d84468b1c606a343f32ca36551c
Alihan Doğuş Yalçın <alihandogus.yalcin@masomo.com>
2021-01-21 19:49:45 +0300

    version update

--- nats-spy.rb
+++ nats-spy.rb
@@ -3,7 +3,7 @@ class NatsSpy < Formula
    homepage "https://github.com/alihanyalcin/nats-spy"
    url "https://github.com/alihanyalcin/nats-spy/archive/v0.1.tar.gz"
    sha256 "78df43d600a5d83aef7778065403e392750164d475fce6fbf296d1f98c8d1621"
-   version "v0.1"
+   version "v0.1.1"
 
    def install
      bin.install "nats-spy"
