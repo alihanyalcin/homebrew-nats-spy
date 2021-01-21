9b1760cb5ccfb8a5501c7fedb42d42f17cc65ddb
Alihan Doğuş Yalçın <alihandogus.yalcin@masomo.com>
2021-01-21 19:40:16 +0300

    formula for v0.1

---
+++ nats-spy.rb
@@ -0,0 +1,15 @@
+class NatsSpy < Formula
+   desc "nats-spy is a terminal tool to help you to monitor NATS messages."
+   homepage "https://github.com/alihanyalcin/nats-spy"
+   url "https://github.com/alihanyalcin/nats-spy/archive/v0.1.tar.gz"
+   sha256 "78df43d600a5d83aef7778065403e392750164d475fce6fbf296d1f98c8d1621"
+   version "v0.1"
+
+   bottle :unneeded
+
+   def install
+     bin.install "nats-spy"
+   end
+ 
+end
+ 
