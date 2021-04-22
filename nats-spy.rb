45911f16c2bdf27d396b4977f1507992868eee8c
alihan.yalcin <alihan.yalcin@trendyol.com>
2021-04-22 22:59:47 +0300

    macos support

--- nats-spy.rb
+++ nats-spy.rb
@@ -1,11 +1,13 @@
 class NatsSpy < Formula
    desc "nats-spy is a terminal tool to help you to monitor NATS messages."
    homepage "https://github.com/alihanyalcin/nats-spy"
-   url "https://github.com/alihanyalcin/nats-spy/releases/download/v0.1.0/nats-spy-mac.tar.gz"
-   sha256 "94949978f9d7a1208276d2ee743692520a452956532d0ca6854c9c36850a3e6f"
    version "v0.1.0"
-
    bottle :unneeded
+
+   if OS.mac?
+     url "https://github.com/alihanyalcin/nats-spy/releases/download/v0.1.0/nats-spy-mac.tar.gz"
+     sha256 "94949978f9d7a1208276d2ee743692520a452956532d0ca6854c9c36850a3e6f"
+   end
 
    def install
      bin.install "nats-spy"
